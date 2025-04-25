7441bb3d87c997b89ca8d0e5336e612d39bc43bd
Joaquín Ballesteros Velázquez <jobave589@gmail.com>
2025-04-25 23:18:55 +0200

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -15,7 +15,7 @@ st.title(f'Red de Cargadores Madrid 2024')  # Título principal de la aplicació
 
 st.image('madrid_skyline.jpg',
          caption='Madrid Skyline',
-         width=1200)
+         width=800)
 
 # Carga y procesamiento de datos
 try:
